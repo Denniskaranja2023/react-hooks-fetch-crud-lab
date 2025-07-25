91a4576822717cddd395c9765732af558145a4df
Denniskaranja2023! <denniswanyeki2021@gmail.com>
2025-07-25 13:23:20 +0300

    make change on question list

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,35 +1,58 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
- const [questions,setQuestions]= useState([])
+  const [questions, setQuestions] = useState([]);
 
-  useEffect(()=>{
+  // Fetch questions on mount
+  useEffect(() => {
+    let isMounted = true;
+
     fetch("http://localhost:4000/questions")
-    .then(res=>res.json())
-    .then((questions)=>setQuestions(questions))
-  },[questions])
+      .then((res) => res.json())
+      .then((data) => {
+        if (isMounted) {
+          setQuestions(data);
+        }
+      })
+      .catch((error) => console.error("Error fetching questions:", error));
 
-   function handleDelete(event){
-    const id = event.target.id
-    fetch(`http://localhost:4000/questions/${id}`,
-      {
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  // Delete handler
+  function handleDelete(event) {
+    const id= event.target.id
+    fetch(`http://localhost:4000/questions/${id}`, {
       method: "DELETE",
-    }).then(res=>res.json()).then(()=>{
-      setQuestions((questions)=>questions.filter((q)=>q.id !== id))
     })
+      .then((res) => {
+        if (!res.ok) throw new Error("Failed to delete");
+        // No need to parse JSON on DELETE if no body returned
+        setQuestions((prevQuestions) =>
+          prevQuestions.filter((q) => q.id !== parseInt(id))
+        );
+      })
+      .catch((error) => console.error("Error deleting question:", error));
   }
-    
- 
 
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{questions.map((question)=>{
-       return <QuestionItem question={question} deleteQuestion={handleDelete}/>
-      })}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            deleteQuestion={handleDelete}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
 
 export default QuestionList;
+
